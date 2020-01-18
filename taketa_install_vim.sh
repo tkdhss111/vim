3ec2bbe0a3e408d1ab1cdb8163fbfcd5ee27191d
tkdhss111 <tkdhss111@gmail.com>
2020-01-18 10:22:34 +0900

    git

--- taketa_install_vim.sh
+++ taketa_install_vim.sh
@@ -146,7 +146,7 @@ git push
 # git config remote.origin.url
 # git remote set-url origin git@github.com:tkdhss111/[リポジトリ].git
 # DO NOT USE : git clone https... 
-# BUT USE WHEN SSH: git clone git@github.com/tkdhss111/[リポジトリ].git
+# BUT USE WHEN SSH: git clone git@github.com:tkdhss111/[リポジトリ].git
 
 #
 # Fortran syntax highlighting
