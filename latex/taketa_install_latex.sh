59f3df6cd64aaa68c38cd27020dd512c1591276d
tkdhss111 <tkdhss111@gmail.com>
2020-02-02 16:57:39 +0900

    latex

--- latex/taketa_install_latex.sh
+++ latex/taketa_install_latex.sh
@@ -1,7 +1,7 @@
 apt install texlive-lang-cjk xdvik-ja texlive-fonts-recommended texlive-fonts-extra -y
 apt install latexmk -y
 cp .latexmkrc ~/.latexmkrc
-apt install zathura xdotool -y
+apt install zathura libsynctex-dev libgtk-3-dev xdotool -y
 mkdir -p ~/.config/zathura/
 cp .zathurarc ~/.config/zathura/.zathurarc
 pip install --upgrade pip
