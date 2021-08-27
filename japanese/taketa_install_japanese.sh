3b5c80aafaf9083997296a2b5bf0515c7aef2661
tkdhss111 <tkdhss111@gmail.com>
2021-08-27 17:27:27 +0900

    delete unused scripts

--- japanese/taketa_install_japanese.sh
+++
@@ -1,7 +0,0 @@
-sudo apt -y install fcitx-mozc
-sudo reboot
-Menu - Settings - Ficitix Configuration - Input Method
-Press [+] botton
-Uncheck [x] and enter 'mozc'.
-
-https://hnakamur.github.io/blog/2020/06/11/setup-fcitx-mozc-on-ubuntu-20.04-desktop/
