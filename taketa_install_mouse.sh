5d0fd4e389d51b8c5f7ec771563208247827937e
tkdhss111 <tkdhss111@gmail.com>
2021-08-29 18:45:23 +0900

    mouse bug

--- taketa_install_mouse.sh
+++ taketa_install_mouse.sh
@@ -1,16 +1,19 @@
 # https://askubuntu.com/questions/236529/binding-back-forward-to-mouse-buttons
+# TWICE CLICK BUG https://forum.obsidian.md/t/linux-mouse-button-goes-back-twice/748/2
 
-sudo apt install -y x11-utils xautomation
+sudo apt install -y xdotool xautomation
 
 cat <<EOF>~/.xbindkeysrc
 #
 # taketa mouse back and forward configuration
 #
 # History Back & Forward
-"xte 'keydown Alt_L' 'keydown Left' 'keyup Left' 'keyup Alt_L'"
+#"xte 'keydown Alt_L' 'keydown Left' 'keyup Left' 'keyup Alt_L'"
+"xdotool key alt+Left"
      b:6
 
-"xte 'keydown Alt_L' 'keydown Right' 'keyup Right' 'keyup Alt_L'"
+#"xte 'keydown Alt_L' 'keydown Right' 'keyup Right' 'keyup Alt_L'"
+"xdotool key alt+Right"
      b:7
 EOF
 
